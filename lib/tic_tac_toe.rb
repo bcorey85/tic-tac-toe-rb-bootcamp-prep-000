2d67a5e30d5ddbf903c9487931fb7b25b7ded6f9
brandon corey <bcorey85@gmail.com>
2019-04-11 22:56:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,13 +39,6 @@ def valid_move?(board, index)
 end
 
 def turn(board)
-<<<<<<< HEAD
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-=======
   puts "Please enter: 1-9"
   input = gets.strip
   index = input_to_index(input)
