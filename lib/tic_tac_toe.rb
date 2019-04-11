cbf807c71671482446ee9691e9f4d887329c56ce
brandon corey <bcorey85@gmail.com>
2019-04-11 22:56:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,9 +25,6 @@ end
 
 def move(board, index, char)
   !position_taken?(board, index) ? board[index] = char : nil
-  if !position_taken?(board, index)
-    board[index] = char
-  end
 end
 
 def position_taken?(board,index)
