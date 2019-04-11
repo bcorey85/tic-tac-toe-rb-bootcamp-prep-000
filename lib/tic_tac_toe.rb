1c4dfb8af932965e8dc117c397770ffcb3eeca0e
brandon corey <bcorey85@gmail.com>
2019-04-11 23:00:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,6 +49,18 @@ def turn(board)
   end
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
 def turn_count(board)
   counter = 0
   board.each do |e|
